8386987826ba2bb0f49e88ef81166b6224d417fc
tdurieux <durieuxthomas@hotmail.com>
2016-04-22 17:05:11 +0200

    protects json parse

--- src/main/javascript/importLog.js
+++ src/main/javascript/importLog.js
@@ -42,7 +42,7 @@ MongoClient.connect(urlDataset, function(err, db) {
         var countLog = 0;
         const builds = data.builds;
         delete data.builds;
-        async.eachSeries(builds, function (build, callback) {
+        async.eachLimit(builds, 8, function (build, callback) {
             db.collection('repos').findOne({
                 "id": build.id
             }, function (err, result) {
@@ -53,25 +53,29 @@ MongoClient.connect(urlDataset, function(err, db) {
                     return callback();
                 }
                 var logs = {};
-                async.each(build.job_ids, function (job_id, callbackJob) {
+                async.eachLimit(build.job_ids, 3, function (job_id, callbackJob) {
                     getLog(job_id).then(function (log) {
                         countLog++;
                         request.post({url: 'http://localhost:7070/', body: log}, function(error, httpResponse) {
                             if (error) {
                                 return callbackJob(err);
                             }
-                            const jsOutput = JSON.parse(httpResponse.body);
-                            if (!jsOutput) {
-                                return callbackJob();
-                            }
-                            logs[job_id] = jsOutput;
-                            for (var i = 0; i < jsOutput.length; i++) {
-                                if (!exceptionTypes[jsOutput[i].exceptionType]) {
-                                    exceptionTypes[jsOutput[i].exceptionType] = 1;
-                                } else {
-                                    exceptionTypes[jsOutput[i].exceptionType] ++;
+                            try {
+                                const jsOutput = JSON.parse(httpResponse.body);
+                                if (!jsOutput) {
+                                    return callbackJob();
                                 }
-                                console.log(jsOutput[i].exceptionType, exceptionTypes[jsOutput[i].exceptionType]);
+                                logs[job_id] = jsOutput;
+                                for (var i = 0; i < jsOutput.length; i++) {
+                                    if (!exceptionTypes[jsOutput[i].exceptionType]) {
+                                        exceptionTypes[jsOutput[i].exceptionType] = 1;
+                                    } else {
+                                        exceptionTypes[jsOutput[i].exceptionType]++;
+                                    }
+                                    console.log(jsOutput[i].exceptionType, exceptionTypes[jsOutput[i].exceptionType]);
+                                }
+                            } catch (e) {
+                                console.error(e);
                             }
                             callbackJob();
                         });
